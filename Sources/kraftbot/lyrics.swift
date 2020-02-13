2324a7a5608a6b3d2f156e8a91011d251eca324e
Lino <linonava@patikerm.com>
2020-02-13 00:04:51 -0500

    try

--- Sources/kraftbot/lyrics.swift
+++ Sources/kraftbot/lyrics.swift
@@ -2,17 +2,35 @@
 //  test.swift
 //  kraftbot
 //
-//  Created by Lino Nava on 2020-02-04.
 //
 
 import Foundation
 import Tracery
 
 func sing()->String{
-    var t = Tracery {[
-        "name": ["calc", "auto", "non"],
-        "emoji": ["🤖","📺","🚵‍♀️"],
-        "msg": "#name# is #age# years old",
+    let t = Tracery {[
+        "autoemoji": ["🛣"],
+        "pcemoji":["➕","➗","📱","➖"],
+        "tdfemoji":["🚲","🇫🇷","🏔"],
+        "tremoji":["🤖","⚡️","🔋"],
+        
+        "song":[
+            "#pcemoji# #pocketcalculator# #pcemoji#",
+            "#autoemoji# #autobahn# #autoemoji#",
+            "#tdfemoji# #tourdefrance# #tdfemoji#",
+            "#tremoji# #therobots# #tremoji#",
+            
+        ],
+        
+        "autobahn":["Autobahn","Wir fahr'n, fahr'n, fahr'n auf der Autobahn","Vor uns liegt ein weites Tal","Die Sonne scheint mit Glitzerstrahl","Die Fahrbahn ist ein graues Band","Weiße Streifen, grüner Rand"],
+        
+        "pocketcalculator":["Bokuwa ongakuka","dentaku katateni","Sousashite","I'm the operator with my pocket calculator","I am adding","And subtracting","By pressing down a special key","It plays a little melody"],
+        
+        "tourdefrance":["L'enfer de Nord-Paris, Roubaix","La Cote d'Azur et St Tropez","Les Alpes et les Pyrénées","Le dernière etape, Champs-Elysées","Galibier et Tourmalet","Tour de France, Tour de France","Pédaler en grand braquet","Le vélo vite réparé","Le peloton est regroupé"],
+        
+        "therobots":["We're charging our battery","And now we're full of energy","We are the robots","We're functioning automatic","And we are dancing mechanic","Я твой слуга","Я твой работник","We are programmed just to do","Anything you want us to"],
+        
+        "msg": "#song#",
     ]}
 
     return(t.expand("#msg#"))
--- Sources/kraftbot/main.swift
+++ Sources/kraftbot/main.swift
@@ -9,29 +9,15 @@ let CONSUMER_SECRET = env.get("CONSUMER_SECRET")!
 let ACCESS_TOKEN = env.get("ACCESS_TOKEN")!
 let ACCESS_SECRET = env.get("ACCESS_SECRET")!
 
-print(sing())
-exit(EXIT_SUCCESS)
-
-// Disable for now
-
-/*
 var swifter = Swifter(consumerKey: CONSUMER_KEY, consumerSecret:CONSUMER_SECRET, oauthToken: ACCESS_TOKEN, oauthTokenSecret:ACCESS_SECRET)
 
-swifter.getHomeTimeline(count: 5, success: { json in
-    print(json)
-    exit(EXIT_SUCCESS)
-}, failure: { error in
-    print(error)
-    exit(EXIT_SUCCESS)
-})
-*/
-
-/* to post message
-swifter.postTweet(status: "🤖", success: { status in
+swifter.postTweet(status: sing(), success: { status in
+    print("success")
     print(status)
+    exit(EXIT_SUCCESS)
 }, failure: { error in
-    print(error)
+    print("fail")
+    exit(EXIT_SUCCESS)
 })
-*/
 
 RunLoop.main.run()
